94090eb92dad5e0752550ac28b867cfaf99d19ee
Muhammad Ibrahim Afzal <ibrahimafzal00@hotmail.com>
2026-04-26 02:20:38 +0500

    Day 1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:magic_lens/main.dart';
+import 'package:magic_lens_03/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
